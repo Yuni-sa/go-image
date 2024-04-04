23bfe88d19e7afa6f3bac8dd6d74f94ba731eee7
Yonatan Sasson <yonatanxd7@gmail.com>
2024-04-04 19:22:51 +0300

    refactor: use environment variables for Redis connection configuration

--- database.go
+++ database.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/go-redis/redis"
 )
@@ -9,17 +10,32 @@ import (
 var rdb *redis.Client
 
 func connectDB() *redis.Client {
+
+	redisAddr := "redis-service.socialhub.svc.cluster.local:6379"
+	if r := os.Getenv("REDIS_ADDR"); r != "" {
+		redisAddr = r
+	}
+
+	redisPass := ""
+	if r := os.Getenv("REDIS_PASS"); r != "" {
+		redisAddr = r
+	}
+
 	// defines redis connection
 	rdb = redis.NewClient(&redis.Options{
-		Addr: "localhost:6379",
+		// Addr: "localhost:6379",
 		// Addr:     "redis:6379",
-		Password: "",
+		Addr:     redisAddr,
+		Password: redisPass,
 		DB:       0,
 	})
 
 	// simple ping / connection check
 	pong, err := rdb.Ping().Result()
-	log.Println(pong, err)
+	if err != nil {
+		log.Fatalf("Could not connect to Redis: %v", err)
+	}
+	log.Printf("Redis connected: %s", pong)
 	return rdb
 }
 
